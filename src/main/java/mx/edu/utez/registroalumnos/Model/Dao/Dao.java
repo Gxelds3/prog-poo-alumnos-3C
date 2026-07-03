3ceec8bc2a3042a6348264aec0389185669bd068
MagoDelTribal <20253ds085@utez.edu.mx>
2026-07-03 09:34:50 -0600

    el dao

---
+++ src/main/java/mx/edu/utez/registroalumnos/Model/Dao/Dao.java
@@ -0,0 +1,11 @@
+package mx.edu.utez.registroalumnos.Model.Dao;
+
+import java.util.List;
+
+public interface Dao<T, K> {
+    boolean create (T t);
+    List<T> getAll();
+    T getById(K id);
+    boolean update (T t);
+    boolean delete (T t);
+}
